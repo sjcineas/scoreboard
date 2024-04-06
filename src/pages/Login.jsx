60adc31f2e9a273d592292116e8cc15cb4d02156
sjcineas <101082903+sjcineas@users.noreply.github.com>
2024-04-05 22:01:46 -0400

    Changes to Login UI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,30 +10,57 @@ const Container = styled.div`
     width: 100%;
     height: 100%;
     position: absolute;
-    background-color: #92140C;
+    background-color: #794040;
+    justify-content: center;
+    display: block;
+`
+const BottomOfContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background-color: white;
     justify-content: center;
     display: flex;
 `
+const Banner =  styled.div`
+    height: 20%;
+    width: 100%;
+    display: flex;
+    //background-color: white;
+    align-items: center;
+    justify-content: center;
+`
+const BannerContainer =  styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+`
 
 const LoginContainer = styled.div`
-    margin-top: 150px;
-    width: auto;
-    height: auto;
+    margin-top: 20px;
+    width: 35%;
+    height: 60%;
     position: absolute;
-    background-color: #FFF8F0;
+    //background-color: #FFF8F0;
     border-radius: 20%;
     display: flex; /* Change display to flex */
     flex-direction: column; /* Align items in a column */
     align-items: center; /* Center items horizontally */
-    border: solid 5px;
+    //border: solid 5px;
 `
 const LoginTitle = styled.div`
     font-size: 50px;
+    //font-family:
     text-align: center;
     padding-top: 20px;
     padding-bottom: 30px;
-    width: 100%;
-
+    width: 98%;
+    color: white;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
 
 const InputContainer = styled.div`
@@ -51,6 +78,7 @@ const Inputs = styled.input`
     font-size: 16px;
     font-weight: bold;
     margin-bottom: 25px;
+    background-color: #F4F4F4;
 
 `
 const SubmitButton = styled.button`
@@ -61,21 +89,22 @@ const SubmitButton = styled.button`
     color: #FFF8F0;
     font-weight: bold;
     cursor: pointer;
-    margin-bottom: 20px;
+    margin-bottom: 25px;
     &:hover {
-        background-color: #92140C;
+        background-color: #794040;
   }
 `
 
 const SignUpButton = styled.button`
     border-radius: 4px;
     height: 30px;
+    width: 100%;
     background-color: #1E1E24;
     color: #FFF8F0;
     font-weight: bold;
     cursor: pointer;
     &:hover {
-        background-color: #92140C;
+        background-color: #794040;
   }
 `
 const NewForm = styled.form`
@@ -85,16 +114,36 @@ const NewForm = styled.form`
     width: 100%;
 
 `
+const Image = styled.img`
+    height: 120px;
+    width: 120px;
+`
+const ImageContainer = styled.div`
+    width: 2%;
+    
+`
+
 
 
 const Login = () => {
     return (
      <Container>
         <Announcement/>
+        <Banner>
+            <BannerContainer>
+                <ImageContainer>
+                    <Image src="https://www.nsbe.org/getmedia/9e92a6e6-21d0-4be7-bda9-88b26865694f/1.png" alt=""></Image>
+                </ImageContainer>
+                <LoginTitle>
+                    Login to Scoreboard
+                </LoginTitle>
+            </BannerContainer>
+        </Banner>
+        <BottomOfContainer>
         <LoginContainer>
-            <LoginTitle>
+            {/* <LoginTitle>
                 Login
-            </LoginTitle>
+            </LoginTitle> */}
             <InputContainer>
                 <NewForm>
                     <Inputs placeholder='Username' type='text'></Inputs>
@@ -107,6 +156,8 @@ const Login = () => {
                 </NewForm>
             </InputContainer>
         </LoginContainer>
+        </BottomOfContainer>
+        
      </Container>
     )
   }
