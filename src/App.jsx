3fe6777ac3dd384c808f19a7b96994224c1b8cd2
sjcineas <101082903+sjcineas@users.noreply.github.com>
2024-08-17 03:36:57 -0400

    added icons and changed default homepage

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<CheckPoints/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/checkpoints" element={<CheckPoints/>}/>
--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,18 +1,48 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import FeedIcon from '@mui/icons-material/Feed';
 
 const Container = styled.div`
-    height: 40px;
+    height: 50px;
     background-color: #1E1E24;
     color: white;
-    padding-top: 10px;
+    //padding-top: 10px;
     text-align: center;
     font-weight: bold;
     font-size: 20px;
     display:flex;
     width: 100%;
-    justify-content: center;
+    align-items: center;
+    //justify-content: center;
+`
+const LeftContainer = styled.div`
+    height: 100%;
+    background-color: #1E1E24;
+    color: white;
+    padding-left: 2%;
+    text-align: center;
+    font-weight: bold;
+    font-size: 20px;
+    display:flex;
+    width: 55%;
+    justify-content: right;
+    align-items: center;
+
+`
+const RightContainer = styled.div`
+    height: 100%;
+    background-color: #1E1E24;
+    color: white;
+    //padding-right: 2%;
+    text-align: center;
+    font-weight: bold;
+    font-size: 20px;
+    display:flex;
+    width: 45%;
+    justify-content: right;
+    align-items: center;
 `
 const NewLink = styled(Link)`
     color: white;
@@ -25,12 +55,21 @@ const NewLink = styled(Link)`
     &:active { text-decoration: none; }
 `
 
+const Icon = styled.div`
+    padding-left: 3%;
+    padding-right: 3%;
+`
+
 const Announcement = () => {
   return (
     <Container>
-      <NewLink to="/">
-        SCOREBOARD
-      </NewLink>
+      <LeftContainer>
+      <NewLink to="/">SCOREBOARD</NewLink>
+      </LeftContainer>
+      <RightContainer>
+        <FeedIcon id="FeedIcon" style={{ paddingLeft: '3%', paddingRight: '3%' }} />
+        <AccountBoxIcon id="AccountBoxIcon" style={{ paddingLeft: '3%', paddingRight: '3%' }}/>
+      </RightContainer>
     </Container>
   )
 }
