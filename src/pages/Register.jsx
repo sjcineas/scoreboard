da2609e6d7a9865183450513d50409602a12f736
sjcineas <101082903+sjcineas@users.noreply.github.com>
2025-08-25 12:06:54 -0400

    fixed linting

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import { Link, useNavigate } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
