c4f6f3805f7da985312a1058398d45c30f9f7a10
sjcineas <101082903+sjcineas@users.noreply.github.com>
2025-09-08 23:56:05 -0400

    remove dup

--- src/pages/StudentTable.jsx
+++ src/pages/StudentTable.jsx
@@ -3,8 +3,6 @@ import axios from "axios";
 import styled from "styled-components";
 import NavBar from "../components/NavBar";
 import { Link } from "react-router-dom";
-import { Link } from "react-router-dom";
-
 const Container = styled.div`
   width: 100%;
   min-height: 100vh;
